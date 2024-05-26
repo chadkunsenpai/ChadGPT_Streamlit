a6a4eab0412f492b0c05d95ef4f466de48210cef
John Richard Palmos <99818180+chadkunsenpai@users.noreply.github.com>
2024-05-26 23:41:37 +0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def info():
+    text = """Developer: **John Richard Palmos** (BSCS 3B)\n
+    A Final Project for CCS 229 - Intelligent Systems
+    Bachelor of Science in Computer Science
+    College of Information and Communications Technology
+    West Visayas State University
+    """
+    st.markdown(text)
